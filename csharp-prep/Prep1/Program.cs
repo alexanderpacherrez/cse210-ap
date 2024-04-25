8455570610c9ec730bf050661d63fe0f70eeb7d0
alexanderpacherrez <pacherrezberru@gmail.com>
2024-04-25 12:55:10 -0500

    program two

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,9 +7,9 @@ class Program
         Console.WriteLine("Hello Prep1 World!");
         Console.WriteLine("This is my first program, so");
         Console.Write("What is your first name? ");
-        string first_name  = Console.ReadLine();
+        string firstName  = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string last_name  = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+        string lastName  = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
